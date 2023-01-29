695082161ea2afa703d9c05951e0740643aabaa5
Vitaly <vit-krivenko@yandex.ru>
2023-01-29 15:23:30 +0300

    Добавлена блок схема

--- Kontrol_Task_1.cs
+++ Kontrol_Task_1.cs
@@ -1,4 +1,5 @@
-﻿string[] myArray = {"I", "quick", "brown", "win", "jumps", "and", "tennis", "lazy", "I","those","am","the","man"};
+﻿string[] myArray = {"I", "quick", "brown", "win", "jumps", "and", "tennis", "lazy",
+ "I","those","am","the","man"};
 PrintArray(myArray);
 Console.WriteLine();
 Console.WriteLine("Новый массив состоящий только из строк длиной <= 3 символа:");
@@ -7,18 +8,18 @@ PrintArray(NewShortArray(myArray));
 
 string[] NewShortArray(string[] array)
 {
-    string[] shotArray = new string[array.Length];
+    string[] shortArray = new string[array.Length];
     int j = 0;  
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3) 
         {
-            shotArray[j] = array[i];
+            shortArray[j] = array[i];
             j++;
         } 
     }
-    Array.Resize(ref shotArray, j);
-    return shotArray;
+    Array.Resize(ref shortArray, j);
+    return shortArray;
 }
 
 
